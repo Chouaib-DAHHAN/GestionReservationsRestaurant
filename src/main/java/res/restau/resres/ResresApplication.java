74b9f2fa3f5a2931695e68b5a1e6a788a16fb5c0
Chouaib-DAHHAN <chouaib10md@gmail.com>
2025-06-30 22:21:13 +0100

    commit : create Project

---
+++ src/main/java/res/restau/resres/ResresApplication.java
@@ -0,0 +1,13 @@
+package res.restau.resres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/res/restau/resres/ResresApplicationTests.java
@@ -0,0 +1,13 @@
+package res.restau.resres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
